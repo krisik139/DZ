160807efc6a19d461abb2fbe1b9a8f29d4d7ce3a
krisik139 <sirenkomaksym95@gmail.com>
2026-06-17 16:31:48 +0300

    bla

--- Cycleshz.cpp
+++ Cycleshz.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
-#include <vector>
-
-void flip(std::vector<int>& arr, int k) {
-    for (int i = 0; i <= k / 2; ++i) {
-        int temp = arr[i];
-        arr[i] = arr[k - i];
-        arr[k - i] = temp;
-    }
-}
-
-void pancakeSort(std::vector<int>& arr) {
-    int n = arr.size();
-    for (int curr_size = n; curr_size > 1; --curr_size) {
-        int max_idx = 0;
-        for (int i = 0; i < curr_size; ++i) {
-            if (arr[i] > arr[max_idx]) {
-                max_idx = i;
-            }
-        }
-        if (max_idx != curr_size - 1) {
-            if (max_idx != 0) {
-                flip(arr, max_idx);
-            }
-            flip(arr, curr_size - 1);
-        }
-    }
-}
 
 int main() {
-    std::vector<int> pancakes = { 3, 1, 5, 2, 4 };
+    int num = 0;
+    int reversed = 0;
+    std::cout << "Введіть число: ";
+    std::cin >> num;
 
-    pancakeSort(pancakes);
-
-    for (int i = 0; i < pancakes.size(); i++) {
-        std::cout << pancakes[i] << " ";
+    while (num != 0) {
+        reversed = reversed * 10 + (num % 10); // формула щоб отримати останню цифру числа
+        num /= 10;
     }
 
+    std::cout << "Перевернуте число: " << reversed << std::endl;
+
+    // Завдання 2
+
+    num = 0;
+    int maxDigit = 0;
+    std::cout << "Введіть число: ";
+    std::cin >> num;
+
+    while (num > 0) {
+        int currentDigit = num % 10; // отримуємо останню цифру
+        if (currentDigit > maxDigit) {
+            maxDigit = currentDigit;
+        }
+        num /= 10;
+    }
+
+    std::cout << "Найбільша цифра: " << maxDigit << std::endl;
     return 0;
 }
